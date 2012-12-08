1f7f0b79be12e743d97e79b0344271df777f0a49
Erik Dietrich <erik@daedtech.com>
2012-12-07 18:58:56 -0600

    Got initial query for entity/field working

--- AutotaskShell/Program.cs
+++ AutotaskShell/Program.cs
@@ -15,16 +15,24 @@ namespace AutotaskShell
             string userName = GetUserName();
             string pass = GetPassword();
             var service = LoginToAutotask(userName, pass);
+            Console.Write(">");
 
-            const string query = "<queryxml><entity>Account</entity><query><field>id<expression op=\"greaterthan\">0</expression></field></query></queryxml>";
-            var result = service.query(query);
-            if (result.EntityResults.Any())
-                result.EntityResults.Cast<Account>().ToList().ForEach(acc => Console.WriteLine("Account name is " + acc.AccountName));
-            else
-                Console.WriteLine("Error");
+            for (string command = Console.ReadLine(); command.ToLower() != "exit"; command = Console.ReadLine())
+            {
+                string entity = command.Split('.')[0];
+                string field = command.Split('.')[1];
 
-            Console.ReadLine();
+                string query = string.Format("<queryxml><entity>{0}</entity><query><field>id<expression op=\"greaterthan\">0</expression></field></query></queryxml>", entity);
+                var results = service.query(query);
+                results.EntityResults.Cast<Account>().ToList().ForEach(acc => Console.WriteLine("Account name is " + acc.AccountName));
 
+                foreach (var result in results.EntityResults)
+                {
+                    Console.WriteLine(result.GetType().GetProperty(field).GetValue(result, null));
+                }
+
+                Console.Write(">");                
+            }
         }
 
         private static string GetUserName()
